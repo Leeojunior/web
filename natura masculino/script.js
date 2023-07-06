406f424fca5412820ddf751de320fc937085d230
Leonardo Junior <129981309+Leeojunior@users.noreply.github.com>
2023-07-06 11:40:55 -0300

    Add files via upload

---
+++ natura masculino/script.js
@@ -0,0 +1,16 @@
+//efeito do botão voltar ao Topo
+function topo(){
+
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:"smooth"
+        }
+  
+    )
+  
+  }
+
+
+
